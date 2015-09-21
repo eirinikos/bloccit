72f6fddd36eeca549f3e6067dd751d723c04f736
Andrea <eirinikos@gmail.com>
2015-09-20 23:35:52 -0700

    assignment 47 completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,16 @@
 class CommentsController < ApplicationController
   def new
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new
     authorize @comment
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])  
     @comment = Comment.new(params.require(:comment).permit(:body))
     @comment.user = current_user
     @comment.post = @post
+
     authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
@@ -23,17 +22,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed!"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again!"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 end

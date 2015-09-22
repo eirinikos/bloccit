3dfe90bf267fd59de3acda8b530aa2b4a7f2b570
Andrea <eirinikos@gmail.com>
2015-09-21 21:56:52 -0700

    fixed typo & added expectation for invalid vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,12 +5,14 @@ describe Vote do
   describe "validation" do
 
     before do
-      @pvalid_vote = Vote.create(value: -1)
+      @valid_vote = Vote.create(value: -1)
+      @invalid_vote = Vote.create(value: 3)
     end
 
     describe '#valid?' do
       it "only allows -1 or 1 as values" do
         expect( @valid_vote.valid?).to eq(true)
+        expect( @invalid_vote.valid?).to eq(false)
       end
     end
   end

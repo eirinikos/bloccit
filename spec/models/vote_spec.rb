d6c3c4a47ce30ae9ada36a566f1ab49bdcd19695
Andrea <eirinikos@gmail.com>
2015-09-21 21:54:56 -0700

    updated to include before block in vote_spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 describe Vote do
   
   describe "validation" do
+
+    before do
+      @pvalid_vote = Vote.create(value: -1)
+    end
+
     describe '#valid?' do
       it "only allows -1 or 1 as values" do
         expect( @valid_vote.valid?).to eq(true)
       end
     end
   end
-  
+
 end

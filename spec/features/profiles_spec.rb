843db3579b6d968bbf652e0b0a9cbd32649df190
Andrea <eirinikos@gmail.com>
2015-10-04 22:29:41 -0700

    assignment 58 completed

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe "visiting profiles" do
   
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
-    # this rand user is not signed in
+    # this user is not signed in
     # test user is nil
     @post = associated_post(user: @user)
     @comment = Comment.new(user: @user, body: "A Comment")
@@ -14,7 +16,23 @@ describe "visiting profiles" do
     @comment.save!
   end
 
-  describe "- not signed in -" do
+  describe "- while signed in -" do
+    
+    before do
+      login_as(authenticated_user, scope: :user)
+    end
+
+    it "shows profile" do
+      visit(user_path(@user))
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
+  end
+
+  describe "- while not signed in -" do
     it "shows profile" do
       visit(user_path(@user))
       expect(current_path).to eq(user_path(@user))

562c2948c3fb191030f95bd76c5f96e17ad9445e
Andrea <eirinikos@gmail.com>
2015-10-09 15:58:35 -0700

    assignment 60 in progress

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,12 +5,13 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+  end
 
-    factory :user_with_post_and_comment, class: User do
-      after(:build) do |user|
-        create(:post, user: user_with_post_and_comment)
-        create(:comment, user: user_with_post_and_comment)
-      end
-    end
+  after(:build) do |user|
+    create(:post, user: user_with_post_and_comment)
+    create(:comment, user: user_with_post_and_comment)
+  end
+  
+  factory :user_with_post_and_comment, class: User do
   end
 end
